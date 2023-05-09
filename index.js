e1b33b7c7b9d01b50009dffe84668f8220293e1d
Shubham Shah <shubhamshah2203@gmail.com>
2023-05-09 19:36:35 +0530

    All Files Upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// Counter Program
+
+let count = 0;
+
+document.getElementById("increase").onclick = function(){
+    count += 1;
+    document.getElementById("counter").innerHTML = count;
+}
+
+document.getElementById("decrease").onclick = function(){
+    count -= 1;
+    document.getElementById("counter").innerHTML = count;
+}
+
+document.getElementById("reset").onclick = function(){
+    count = 0;
+    document.getElementById("counter").innerHTML = count;
+}
